474de28f2cb1191fed089f0ffe3f52f8d5991721
周朕威 <zhouzhenwei@ysten.com>
2018-07-25 16:43:55 +0800

    #1 上传文件

---
+++ Example/YSTKtvKit/YSTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YSTAppDelegate.h
+//  YSTKtvKit
+//
+//  Created by kuqiqi on 06/12/2018.
+//  Copyright (c) 2018 kuqiqi. All rights reserved.
+//
+
+@import UIKit;
+
+@interface YSTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/YSTKtvKit/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  YSTKtvKit
+//
+//  Created by kuqiqi on 06/12/2018.
+//  Copyright (c) 2018 kuqiqi. All rights reserved.
+//
+
+@import UIKit;
+#import "YSTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YSTAppDelegate class]));
+    }
+}
